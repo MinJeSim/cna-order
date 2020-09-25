45a9daa748d03f4ddab939c910c13d08efd52ca0
MinJaeSim <mj9757@gmail.com>
2020-09-25 11:33:45 +0900

    1st commit..

---
+++ src/main/java/mall/Order.java
@@ -0,0 +1,79 @@
+package mall;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Order_table")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String productId;
+    private Integer qty;
+    private String stauts;
+
+    @PostPersist
+    public void onPostPersist(){
+        Ordered ordered = new Ordered();
+        BeanUtils.copyProperties(this, ordered);
+        ordered.publishAfterCommit();
+    }
+
+    @PostUpdate
+    public void onPostUpdate(){
+        System.out.println("test");
+    }
+
+    @PreRemove
+    public void onPreRemove(){
+        OrderCancelled orderCancelled = new OrderCancelled();
+        BeanUtils.copyProperties(this, orderCancelled);
+        orderCancelled.publishAfterCommit();
+
+        //Following code causes dependency to external APIs
+        // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
+
+        mall.external.Cancellation cancellation = new mall.external.Cancellation();
+        // mappings goes here
+        cancellation.setOrderId(this.getId());
+        cancellation.setStatus("CANCELED");
+        OrderApplication.applicationContext.getBean(mall.external.CancellationService.class)
+            .cancel(cancellation);
+
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    public String getStauts() {
+        return stauts;
+    }
+
+    public void setStauts(String stauts) {
+        this.stauts = stauts;
+    }
+}
